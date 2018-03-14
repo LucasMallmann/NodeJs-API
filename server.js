e65326e9066213456a01071121dfc04535620d05
Lucas Mallmann <lucasmallmann76@gmail.com>
2018-03-14 10:26:29 -0300

    Serve iniciado e funcionando com Sucesso

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./app');
+
+// Variável de ambiente para acessar a porta.
+// Se não estiver setada, use a porta 3000
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
