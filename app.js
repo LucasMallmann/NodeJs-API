26e19965ad3717a6885994e63d1be88814eac493
Lucas Mallmann <lucasmallmann76@gmail.com>
2018-03-14 22:04:18 -0300

    new routes

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+// Lib to log requests
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // Tudo o que começar com '/products' deve ser redirecionado para o arquivo ./api/routes/products
+// Routes that handle requests
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// This line will be reached if no route was found
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
