352283f0c16c877e2d39009c33923fea8f85510b
Lucas Mallmann <lucasmallmann76@gmail.com>
2018-03-14 22:04:32 -0300

    update

---
+++ api/routes/orders.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+// GEt
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET orders'
+    });
+});
+
+// Post
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    });
+});
+
+// GEt com id 
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        id: req.params.orderId
+    });
+});
+
+router.patch('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        messsage: 'Order updated',
+        id: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted',
+        id: req.params.orderId
+    });
+});
+
+
+module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -5,13 +5,41 @@ const router = express.Router();
 // Isso é como se fosse uma sobrota, que irá acionar quando bater /products
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        messsage: 'Handling GET requests to /products'
+        messsage: "Handling GET requests to /products"
     });
 });
 
+// Rota para Post
 router.post('/', function(req, res, next){
-    res.status(200).json({
+    res.status(201).json({
         messsage: "Handling POST requests to /products"
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            messsage: 'You have discovere a special Id ;D',
+            id: id
+        });
+    }
+    else{
+        res.status(200).json({
+            messsage: 'You passed an id'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        messsage: 'Updated product'
+    });
+});
+
+router.delete('/:productId', function(req, res, next) {
+    res.status(200).json({
+        messsage: 'Product deleted'
     });
 });
 
