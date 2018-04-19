a1214f9b0f90513594e46095de3a8f63b31f2eaf
Lucas Mallmann <lucasmallmann76@gmail.com>
2018-04-18 21:37:00 -0300

    Token is got from the Header, and not from the body.

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -3,15 +3,18 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
 
     try {
+        // preciso pegar a token do header, e não do Body
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         // verifica e valida o token
-        const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.body.userData = decoded;
         // chame o next se a autorização funcionar
         next();
 
     } catch (error) {
         return res.status(401).json({
-          message: 'Token not valid'
+          message: 'Auth failed'
         });
     }
 }
